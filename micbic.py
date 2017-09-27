8c1efc608d8132b55b75243f43eaf7f5f4626498
rrumb <rocketrumble@mail.ru>
2017-09-27 11:38:14 -0500

    0.0

---
+++ micbic.py
@@ -0,0 +1,31 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start (bot, update):
+    upate.message.reply_text('mic bic!!')
+
+def hello (bot, update):
+    update.message.reply_text('hey, {}'.format(update.message.from_user.first_name))
+
+def echo (bot, update):
+    update.message.reply_text(update.message.text)
+
+
+
+
+def main():
+    updater = Updater('472133412:AAG6XtiiuAqy26RdyJaGRSNsuk-EMnDEpNU')
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('hello', hello))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
+
